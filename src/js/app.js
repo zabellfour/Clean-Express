338466124a275753dee00e3a799a8d7db361115e
zabellfour <zabell2814@gmail.com>
2016-12-03 00:19:15 +0200

    added header

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,7 @@ import HP from './modules/helpers';
 
     // When DOM is ready
     $(function() {
-        $(window).stellar();
-        $('.datepicker').pickadate();
-        $('.owl-carousel').owlCarousel({
-        	items:1,
-        	loop:true,
-        	dots:true,
-        	autoplay:true
-        });
+       
     });
 
 }(jQuery));
